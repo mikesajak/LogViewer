e6b07e3d4bf39572c0a3ee692b8ac02566179f29
mike <mike.sajak@gmail.com>
2018-11-15 20:40:17 +0100

    Interval tree works.

--- src/main/scala/org/mikesajak/logviewer/util/collection/IntervalTreeSet.scala
+++ src/main/scala/org/mikesajak/logviewer/util/collection/IntervalTreeSet.scala
@@ -3,42 +3,47 @@ package org.mikesajak.logviewer.util.collection
 import scala.annotation.tailrec
 import scala.collection.mutable
 
-class IntervalStartOrdering[A](implicit ord: Ordering[A]) extends Ordering[Interval[A]] {
-  override def compare(x: Interval[A], y: Interval[A]): Int = ord.compare(x.start, y.start)
+class IntervalStartOrdering[A](implicit ord: Ordering[A]) extends Ordering[IntervalLike[A]] {
+  override def compare(x: IntervalLike[A], y: IntervalLike[A]): Int = ord.compare(x.start, y.start)
 }
 
-case class Interval[A](start: A, end: A)
+trait IntervalLike[A] {
+  val start: A
+  val end: A
+}
+
+//case class Interval[A](start: A, end: A) extends IntervalLike[A]
 
 object IntervalTreeSet {
 
-  sealed abstract class Node[A, B](var level: Int) {
+  sealed abstract class Node[A](var level: Int) {
     def mkStr(indent: String, short: Boolean = false) = s"$toString"
   }
-  case class LeafNode[A, B]() extends Node[A, B](0)
+  case class LeafNode[A]() extends Node[A](0)
 
-  def addTo[A, B](node: Node[A, B], interval: Interval[A], value: B)(implicit ord: Ordering[A], iord: Ordering[Interval[A]]): ValueNode[A, B] = {
+  def addTo[A](node: Node[A], interval: IntervalLike[A])(implicit ord: Ordering[A], iord: Ordering[IntervalLike[A]]): ValueNode[A] = {
 //    println(s"addTo: $interval to ${node.mkStr("", short = true)}")
     node match {
-      case l : LeafNode[A, B] => mkValueNode(interval, value)(ord, iord)
-      case v : ValueNode[A, B] => v.add(interval, value)
+      case l : LeafNode[A] => mkValueNode(interval)(ord, iord)
+      case v : ValueNode[A] => v.add(interval)
     }
   }
 
-  case class ValueNode[A, B](var interval: Interval[A], var maxEnd: A, lvl: Int, valueSet: mutable.Set[B],
-                             var left: Node[A, B], var right: Node[A, B])
-                         (implicit ord: Ordering[A], iord: Ordering[Interval[A]]) extends Node[A, B](lvl) {
+  case class ValueNode[A](var interval: IntervalLike[A], var maxEnd: A, lvl: Int,
+                          var left: Node[A], var right: Node[A])
+                         (implicit ord: Ordering[A], iord: Ordering[IntervalLike[A]]) extends Node[A](lvl) {
 
-    def this(interval: Interval[A], maxEnd: A, level: Int, value: B, left: Node[A, B], right: Node[A, B])
-            (implicit ord: Ordering[A], iord: Ordering[Interval[A]]) =
-      this(interval, maxEnd, level, mutable.Set(value), left, right)
+    def this(interval: IntervalLike[A], maxEnd: A, level: Int, left: Node[A], right: Node[A])
+            (implicit ord: Ordering[A], iord: Ordering[IntervalLike[A]]) =
+      this(interval, maxEnd, level, left, right)
 
     def updateMaxValues(): Unit = {
       left match {
-        case lv: ValueNode[A, B] => lv.updateMaxValue()
+        case lv: ValueNode[A] => lv.updateMaxValue()
         case _ =>
       }
       right match {
-        case rv: ValueNode[A, B] => rv.updateMaxValue()
+        case rv: ValueNode[A] => rv.updateMaxValue()
         case _ =>
       }
       updateMaxValue()
@@ -46,17 +51,17 @@ object IntervalTreeSet {
 
     def updateMaxValue(): Unit = {
       maxEnd = right match {
-        case r: ValueNode[A, B] => ord.max(interval.end, r.maxEnd)
+        case r: ValueNode[A] => ord.max(interval.end, r.maxEnd)
         case _ => interval.end
       }
     }
 
-    def add(intvl: Interval[A], value: B): ValueNode[A, B] = {
+    def add(intvl: IntervalLike[A]): ValueNode[A] = {
       assume(intvl != null)
 //      println(s"add $v to ${mkStr("", short = true)}")
 
-      if (iord.lt(intvl, interval)) left = addTo(left, intvl, value)
-      else if (iord.gt(intvl, interval)) right = addTo(right, intvl, value)
+      if (iord.lt(intvl, interval)) left = addTo(left, intvl)
+      else if (iord.gt(intvl, interval)) right = addTo(right, intvl)
       else throw new IllegalArgumentException(s"""Value \"$intvl\" is already in a tree""""")
 
       val result = split().skew() // rebalance this node
@@ -64,25 +69,25 @@ object IntervalTreeSet {
       result
     }
 
-    def remove(v: Interval[A]): Node[A, B] = {
+    def remove(v: IntervalLike[A]): Node[A] = {
       iord.compare(v, interval) match {
         case x if x < 0 =>
-          left = left.asInstanceOf[ValueNode[A, B]].remove(v)
+          left = left.asInstanceOf[ValueNode[A]].remove(v)
           rebalance()
 
         case x if x > 0 =>
-          right = right.asInstanceOf[ValueNode[A, B]].remove(v)
+          right = right.asInstanceOf[ValueNode[A]].remove(v)
           rebalance()
 
         case _ => // remove value at this node
           this match {
-            case ValueNode(_, _, _, _, lv @ ValueNode(_, _, _, _, _, _), _) =>
+            case ValueNode(_, _, _, lv @ ValueNode(_, _, _, _, _), _) =>
               val bottomRight = deepestRight(lv)
               interval = bottomRight.interval
               left = lv.remove(interval)
               rebalance()
 
-            case ValueNode(_, _, _, _, _, rv @ ValueNode(_, _, _, _, _, _)) =>
+            case ValueNode(_, _, _, _, rv @ ValueNode(_, _, _, _, _)) =>
               val bottomLeft = deepestLeft(rv)
               interval = bottomLeft.interval
               right = rv.remove(interval)
@@ -107,12 +112,12 @@ object IntervalTreeSet {
         level -= 1
 
         var result = this.skew()
-        result.right = result.right.asInstanceOf[ValueNode[A, B]].skew()
-        if (!isLeafNode(result.right.asInstanceOf[ValueNode[A, B]].right))
-          result.right.asInstanceOf[ValueNode[A, B]].right =
-            result.right.asInstanceOf[ValueNode[A, B]].right.asInstanceOf[ValueNode[A, B]].skew()
+        result.right = result.right.asInstanceOf[ValueNode[A]].skew()
+        if (!isLeafNode(result.right.asInstanceOf[ValueNode[A]].right))
+          result.right.asInstanceOf[ValueNode[A]].right =
+            result.right.asInstanceOf[ValueNode[A]].right.asInstanceOf[ValueNode[A]].skew()
         result = result.split()
-        result.right = result.right.asInstanceOf[ValueNode[A, B]].split()
+        result.right = result.right.asInstanceOf[ValueNode[A]].split()
         result.updateMaxValues()
         updateMaxValues()
         result
@@ -121,17 +126,17 @@ object IntervalTreeSet {
 
 
     @tailrec
-    private def deepestRight(node: ValueNode[A, B]): ValueNode[A, B] = {
+    private def deepestRight(node: ValueNode[A]): ValueNode[A] = {
       node.right match {
-        case rv: ValueNode[A, B] => deepestRight(rv)
+        case rv: ValueNode[A] => deepestRight(rv)
         case _ => this
       }
     }
 
     @tailrec
-    private def deepestLeft(node: ValueNode[A, B]): ValueNode[A, B] = {
+    private def deepestLeft(node: ValueNode[A]): ValueNode[A] = {
       node.left match {
-        case lv: ValueNode[A, B] => deepestLeft(lv)
+        case lv: ValueNode[A] => deepestLeft(lv)
         case _ => this
       }
     }
@@ -142,11 +147,11 @@ object IntervalTreeSet {
      *  /   / \        /   / \
      * 0   1   2      0   1   2
      */
-    private def skew(): ValueNode[A, B] = {
+    private def skew(): ValueNode[A] = {
       if (left.level < level) this
       else {
-        assume(left.isInstanceOf[ValueNode[A, B]])
-        val result = left.asInstanceOf[ValueNode[A, B]]
+        assume(left.isInstanceOf[ValueNode[A]])
+        val result = left.asInstanceOf[ValueNode[A]]
         left = result.right
         result.right = this
 
@@ -164,14 +169,14 @@ object IntervalTreeSet {
    *  /   /   / \        / \     / \
    * 0   1   2   3      0   1   2   3
    */
-    private def split(): ValueNode[A, B] = {
+    private def split(): ValueNode[A] = {
       // must short-circuit because if right.level < self.level, then right.right might be null
-      assume(right.isInstanceOf[ValueNode[A, B]])
+      assume(right.isInstanceOf[ValueNode[A]])
 
       if (right.level < level ||
-        (right.asInstanceOf[ValueNode[A, B]].right.level < level)) this
+        (right.asInstanceOf[ValueNode[A]].right.level < level)) this
       else {
-        val result = right.asInstanceOf[ValueNode[A, B]]
+        val result = right.asInstanceOf[ValueNode[A]]
         right = result.left
         result.left = this
         result.level += 1
@@ -185,8 +190,8 @@ object IntervalTreeSet {
     override def toString: String = mkStr("")
 
     override def mkStr(indent: String, short: Boolean): String = {
-      def descr(node: Node[A, B]) = node match {
-        case vn: ValueNode[A, B] => s"ValueNode(${vn.interval})"
+      def descr(node: Node[A]) = node match {
+        case vn: ValueNode[A] => s"ValueNode(${vn.interval})"
         case _ => "LeafNode()"
       }
 
@@ -199,63 +204,46 @@ object IntervalTreeSet {
     }
   }
 
-  def isLeafNode[A, B](node: Node[A, B]): Boolean = node.isInstanceOf[LeafNode[A, B]]
-  def isNotLeafNode[A, B](node: Node[A, B]): Boolean = !isLeafNode(node)
+  def isLeafNode[A](node: Node[A]): Boolean = node.isInstanceOf[LeafNode[A]]
+  def isNotLeafNode[A, B](node: Node[A]): Boolean = !isLeafNode(node)
 
-  def mkLeafNode[A, B](): LeafNode[A, B] = LeafNode()
+  def mkLeafNode[A, B](): LeafNode[A] = LeafNode()
 
-  def mkValueNode[A, B](interval: Interval[A], value: B)(implicit ord: Ordering[A], iord: Ordering[Interval[A]]): ValueNode[A, B] =
-    new ValueNode[A, B](interval, interval.end, 1, value, mkLeafNode(), mkLeafNode())
+  def mkValueNode[A](interval: IntervalLike[A])(implicit ord: Ordering[A], iord: Ordering[IntervalLike[A]]): ValueNode[A] =
+    ValueNode(interval, interval.end, 1, mkLeafNode(), mkLeafNode())
 
 
-  class Iter[A, B](root: Node[A, B]) extends Iterator[IntervalValue[A, B]] {
-    private var stack: List[ValueNode[A, B]] = pushLeft(root, List())
-    private var curNode: ValueNode[A, B] = _
-    private var nodeIterator: Iterator[B] = _
+  class Iter[A](root: Node[A]) extends Iterator[IntervalLike[A]] {
+    private var stack: List[ValueNode[A]] = pushLeft(root, List())
 
     @tailrec
-    private def pushLeft(node: Node[A, B], st: List[ValueNode[A, B]]): List[ValueNode[A, B]] =
+    private def pushLeft(node: Node[A], st: List[ValueNode[A]]): List[ValueNode[A]] =
       node match {
-        case _ : LeafNode[A, B] => st
-        case n: ValueNode[A, B] => pushLeft(n.left, n :: st)
+        case _ : LeafNode[A] => st
+        case n: ValueNode[A] => pushLeft(n.left, n :: st)
       }
 
-    override def hasNext: Boolean =
-      nodeIterator != null && nodeIterator.hasNext ||
-        stack.nonEmpty
+    override def hasNext: Boolean = stack.nonEmpty
 
-    override def next(): IntervalValue[A, B] = {
+    override def next(): IntervalLike[A] = {
       if (!hasNext) throw new NoSuchElementException
       else {
-        if (nodeIterator != null) {
-          val value = nodeIterator.next()
-          if (!nodeIterator.hasNext) {
-            curNode = null
-            nodeIterator = null
-          }
-          IntervalValue(curNode.interval, value)
-        } else {
-          val node = stack.head
-          stack = stack.tail
-          val result = node
-          stack = pushLeft(node.right, stack)
-          nodeIterator = node.valueSet.iterator
-          val value = nodeIterator.next()
-          IntervalValue(result.interval, value)
-        }
+        val node = stack.head
+        stack = stack.tail
+        val result = node
+        stack = pushLeft(node.right, stack)
+        result.interval
       }
     }
   }
 }
 
-case class IntervalValue[A, B](interval: Interval[A], value: B)
-
-class IntervalTreeSet[A, B](implicit ord: Ordering[A], iord: Ordering[Interval[A]])
-    extends mutable.Set[IntervalValue[A, B]] {
+class IntervalTreeSet[A, B](implicit ord: Ordering[A], iord: Ordering[IntervalLike[A]])
+    extends mutable.Set[IntervalLike[A]] {
 
   import IntervalTreeSet._
 
-  var root: Node[A, B] = LeafNode()
+  var root: Node[A] = LeafNode()
   var size0 = 0
 
   override def clear(): Unit = {
@@ -265,33 +253,26 @@ class IntervalTreeSet[A, B](implicit ord: Ordering[A], iord: Ordering[Interval[A
 
   override def size: Int = size0
 
-  override def contains(elem: IntervalValue[A, B]): Boolean = {
-    findInternal(elem.interval) match {
-      case vn: ValueNode[A, B] => vn.valueSet.contains(elem.value)
-      case _ => false
-    }
-  }
-
-  def find(intervalToFind: Interval[A]): Option[ValueNode[A, B]] =
+  def find(intervalToFind: IntervalLike[A]): Option[ValueNode[A]] =
     findInternal(intervalToFind) match {
-      case vn: ValueNode[A, B] => Some(vn)
+      case vn: ValueNode[A] => Some(vn)
       case _ => None
     }
 
-  def contains(intervalToFind: Interval[A]): Boolean =
+  def contains(intervalToFind: IntervalLike[A]): Boolean =
     findInternal(intervalToFind) match {
-      case vn: ValueNode[A, B] => true
+      case vn: ValueNode[A] => true
       case _ => false
     }
 
-  private def findInternal(intervalToFind: Interval[A]): Node[A, B] = {
+  private def findInternal(intervalToFind: IntervalLike[A]): Node[A] = {
     assume(intervalToFind != null)
 
     var node = root
     var found = false
-    while (!node.isInstanceOf[LeafNode[A, B]] && !found) {
+    while (!node.isInstanceOf[LeafNode[A]] && !found) {
       node match {
-        case ValueNode(nodeInterval, maxEnd, level, _, left, right) =>
+        case ValueNode(nodeInterval, maxEnd, level, left, right) =>
           iord.compare(intervalToFind, nodeInterval) match {
             case x if x < 0 => node = left
             case x if x > 0 => node = right
@@ -305,31 +286,31 @@ class IntervalTreeSet[A, B](implicit ord: Ordering[A], iord: Ordering[Interval[A
     node
   }
 
-  override def +=(elem: IntervalValue[A, B]): IntervalTreeSet.this.type = {
+  override def +=(elem: IntervalLike[A]): IntervalTreeSet.this.type = {
     assume(elem != null)
 
     if (size0 == Int.MaxValue) throw new IllegalStateException("Maximum size reached")
     else {
       if (!contains(elem)) {
-        root = addTo(root, elem.interval, elem.value)
+        root = addTo(root, elem)
         size0 += 1
       }
     }
     this
   }
 
-  override def -=(elem: IntervalValue[A, B]): IntervalTreeSet.this.type = {
+  override def -=(elem: IntervalLike[A]): IntervalTreeSet.this.type = {
     assume(elem != null)
 
     if (contains(elem)) {
-      root = root.asInstanceOf[ValueNode[A, B]].remove(elem)
+      root = root.asInstanceOf[ValueNode[A]].remove(elem)
       size0 -= 1
     }
 
     this
   }
 
-  override def iterator: Iterator[IntervalValue[A, B]] = new Iter(root)
+  override def iterator: Iterator[IntervalLike[A]] = new Iter(root)
 
   override def toString = {
     s"IntervalTreeSet($size):\nroot=$root"
